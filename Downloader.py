5ba0d5b28ab6da78e88b283137e9fc41033e8b6d
alvaroruclmesi <43779311+alvaroruclmesi@users.noreply.github.com>
2019-11-20 19:02:20 +0100

    Downloader finalizado

--- Downloader.py
+++ Downloader.py
@@ -1,15 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8; mode: python; -*-
 
+#Imports
 import Ice
-
+Ice.loadSlice('trawlnet.ice')
+import TrawlNet
+import download_mp3 as dwmp3
 import sys
-class Downloader():
 
-    def addDownloadTask(self, url):
-        print("URL recibida ",url)
-        print("descargando")
-        return "Descarga" +url+"finalizada"
-		
+
+class Downloader(TrawlNet.Downloader):
+	#Metodo para descargar la URL
+    def anadirdescarga(self, url):
+        print("URL recibida: ",url)
+        print("Descargando URL: ",url)
+		print("URL descargada: ")
+        return dwmp3.download_mp3(url)
+
+#Servidor
 class Server(Ice.Application):
+	
 	def run(self,argv):
+	
+		downloader = Downloader()
+		comunicador = self.communicator()
+		adaptador = comunicador.createObjectAdapter("Downloader_Adapter")
+		DownloaderAdapter = adapter.add(downloader,comunicador.stringToIdentity("downloader"))
+	
+		print("'{}'".format(DownloaderAdapter),flush=True)
+		adaptador.activate()
+		self.shutdownOnInterrupt()
+		comunicador.waitForShutdown()
+	
+		return 0
+
+server = Server()
+sys.exit(server.main(sys.argv))
---
+++ download_mp3.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8; mode: python; -*-
+
+#Imports
+import os.path
+try:
+	import youtube_dl
+except ImportError:
+	print('Error al importar youtube_dl')
+	sys.exit(1)
+
+class NullLogger:
+	def debug(self,msg): pass
+	def warning(self,msg): pass
+	def error(self,msg): pass
+
+#Opciones de descarga de YouTube
+_YTDL_OPTS_ = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    'logger': NullLogger()
+}
+
+def download_mp3(URL,destino='./'):
+
+	opciones = {}
+	estado = {}
+	
+	def progreso(estado):
+		estado.update(estado)
+	
+	opciones.update(_YTDL_OPTS)
+	opciones['progreso'] = [progreso]
+	opciones['salida'] = os.path.join(destino,'%(title)s.%(ext)s')
+	
+	with youtube_dl.YoutubeDL(options) as youtube:
+		youtube.download([URL])
+	
+	archivo = estado['archivo']
+	archivo = archivo[:archivo.rindex('.') + 1]
+	return archivo + opciones['postprocessors'][0]['preferredcodec']
+	
+		
+	
+	
