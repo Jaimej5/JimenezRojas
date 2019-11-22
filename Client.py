bc6bd27cf98ed68984f00593894fcc9dad6f0650
Jaimej5 <43786797+Jaimej5@users.noreply.github.com>
2019-11-22 10:22:10 +0100

    Actualizacion de cliente
    .

--- Client.py
+++ Client.py
@@ -2,38 +2,26 @@
 # -*- coding: utf-8 -*-
 
 
-import Ice
-Ice.loadSlice('trawlnet.ice')
-import TrawlNet
 import sys
+import Ice # pylint: disable=E0401, C0413
+Ice.loadSlice('trawlnet.ice')
+import TrawlNet # pylint: disable=E0401, C0413
 
-class Client(Ice.Application):
-    def __init__(self):
-        print('Cliente iniciado\n\n')
-
-    def principal(self, current=None):
-        print('-----------------------------------------------------------------')
-        print('|                  Descargar música de YouTube  				   |')
-        print('-----------------------------------------------------------------')
-        print('\nElige una opción:')
-        print('1. Descargar canción')
-        print('2. Salir')
-
-    def descargarSong(self, orchestrator, current=None):
-		print('Introduce la URL de la canción que quieres descargar')
-		name = input()
-		message = orchestrator.downloadTask(name)
-        print('El gestor de descargas responde: ',message,'\n\n')
-
+class Cliente(Ice.Application):
+    '''
+    Cliente
+    '''
     def run(self, argv):
+        '''
+        Iniciando cliente Ice
+        '''
         proxy = self.communicator().stringToProxy(argv[1])
         orchestrator = TrawlNet.OrchestratorPrx.checkedCast(proxy)
-
         if not orchestrator:
-            raise RuntimeError('Invalid orchestrator proxy')
+            raise RuntimeError('Proxy no correcto')
 
-        orchestrator.downloadTask(argv[2])
-        
-		return 0
+        msg = orchestrator.downloadTask(argv[2])
+        print("El cliente recibe ", msg)
+        return 0
 
-sys.exit(Client().main(sys.argv))
+sys.exit(Cliente().main(sys.argv))
