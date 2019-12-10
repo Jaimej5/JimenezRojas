8df24d583e723601cda2c97faea123d9dcbc93dd
alvaroruclmesi <43779311+alvaroruclmesi@users.noreply.github.com>
2019-12-10 23:28:40 +0100

    Importacion E1 y File hash

--- downloader.py
+++ downloader.py
@@ -36,3 +36,66 @@ class DownloaderI(TrawlNet.Downloader):  # pylint: disable=R0903
         event = TrawlNet.UpdateEventPrx.uncheckedCast(orchestrators)
         event.newFile(fileInfo)
         return fileInfo
+    
+class NullLogger:
+    '''
+    NullLogger
+    '''
+    def debug(self, msg):
+        '''
+        debug method
+        '''
+        pass
+
+    def warning(self, msg):
+        '''
+        warning method
+        '''
+        pass
+
+    def error(self, msg):
+        '''
+        error method
+        '''
+        pass
+
+_YOUTUBEDL_OPTS_ = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    'logger': NullLogger()
+}
+
+
+def download_mp3(url, destination='./'):
+    '''
+    Synchronous download from YouTube
+    '''
+    options = {}
+    task_status = {}
+
+    def progress_hook(status):
+        '''
+        progress hook
+        '''
+        task_status.update(status)
+    options.update(_YOUTUBEDL_OPTS_)
+    options['progress_hooks'] = [progress_hook]
+    options['outtmpl'] = os.path.join(destination, '%(title)s.%(ext)s')
+
+    with youtube_dl.YoutubeDL(options) as youtube:
+        youtube.download([url])
+    filename = task_status['filename']
+    filename = filename[:filename.rindex('.') + 1]
+    return filename + options['postprocessors'][0]['preferredcodec']
+
+def file_hash(filename):
+    fileHash = hashlib.sha256()
+    with open(filename, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b''):
+            fileHash.update(chunk)
+    return fileHash.hexdigest()
+
